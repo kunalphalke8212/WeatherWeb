55a7d974546e741bf5b3c7ce653555128b66ed12
kunalphalke8212 <kunalphalke8212@gmail.com>
2024-03-21 11:23:57 +0530

    2 commit

--- WeatherWeb/src/main/java/myWeatherWeb/WeatherWeb.java
+++ WeatherWeb/src/main/java/myWeatherWeb/WeatherWeb.java
@@ -5,8 +5,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
-import java.util.Scanner;
 import java.util.Date;
+import java.util.Scanner;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -48,7 +48,8 @@ public class WeatherWeb extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 		String inputdata=request.getParameter("SearchText");
-		System.out.println(inputdata);
+		//System.out.println(inputdata);
+
 		//API Key
         String apiKey="c8b2fe5fba1ce4bf62b3e23a95fb3573";
         //User Input
@@ -72,66 +73,90 @@ public class WeatherWeb extends HttpServlet {
 
         //scanning
         while(sc.hasNext()) {
-        	
+
         	//scanning line by line
         	responseContent.append(sc.nextLine());
         }
         sc.close();
-        
+
         // Datta is getting in JSON Format(Java scrip Object Notetion)
-        
-       // System.out.println(responseContent);
-        
+
+       //System.out.println(responseContent);
+
         //TypeCastin String To JSON To
-        
+
         Gson gson=new Gson();
         JsonObject jsonobject=gson.fromJson(responseContent.toString(),JsonObject.class);
         System.out.println(jsonobject);
-        
+
         //Separating data from JSON Object to different properties
-        
+
         //date & Time
         long dateTimeTemp=jsonobject.get("dt").getAsLong()*1000;
         String date=new Date(dateTimeTemp).toString();
-        
+
+        long timestamp = jsonobject.get("dt").getAsLong();
+
+      /* // Convert timestamp to LocalDateTime
+        LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), ZoneOffset.UTC);
+        dateTime = dateTime.atZone(ZoneOffset.UTC).withZoneSameInstant(ZoneOffset.ofHoursMinutes(5,34)).toLocalDateTime();
+
+        // Get the date and time from the LocalDateTime object
+        String date = dateTime.toLocalDate().toString();
+        String time = dateTime.toLocalTime().toString();
+
+        // Print the date and time
+        System.out.println("Date: " + date);
+        System.out.println("Time: " + time);*/
+
+
         //Temprecture
         double temperatureKelvin= jsonobject.getAsJsonObject("main").get("temp").getAsDouble();
-        
+
 		int tempreatureCelsius=(int) (temperatureKelvin-273.15);
-		
+
 		//Humidity
         int humidity=jsonobject.getAsJsonObject("main").get("humidity").getAsInt();
-        
+
+        //Pressure
+        int pressure=jsonobject.getAsJsonObject("main").get("pressure").getAsInt();
+
+
+        String country=jsonobject.getAsJsonObject("sys").get("country").toString();
+
+
         //Wind speed
         double windSpeed=jsonobject.getAsJsonObject("wind").get("speed").getAsDouble();
-        
+
         //Weather Condition
-       System.out.println("Before weather condiition check");
+       //System.out.println("Before weather condiition check");
        //String weatherCondition=jsonobject.getAsJsonObject("weather").get(0).getAsJsonObject().get("main").getAsString();
        String weatherCondition = jsonobject.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").getAsString();
        System.out.println(weatherCondition);
-        
+
         //After getting Data into right format seating data for JSP page
-        
+        request.setAttribute("CityName", CityName);
         request.setAttribute("date", date);
+        //request.setAttribute("time",time);
         request.setAttribute("tempreature", tempreatureCelsius);
       //  request.setAttribute("weatherCondition", weatherCondition);
         request.setAttribute("humidity", humidity);
         request.setAttribute("windSpeed", windSpeed);
-        request.setAttribute("weatherCondition ", weatherCondition );
-        request.setAttribute("weatherData", responseContent.toString());
-        
+        request.setAttribute("weatherCondition", weatherCondition );
+        request.setAttribute("pressure", pressure);
+        request.setAttribute("country", country);
+
         // closing the connection
-        
+
         connection.disconnect();
-        System.out.println(date);
+        //System.out.println(date);
         request.getRequestDispatcher("index.jsp").forward(request,response);
         }
         catch(ProtocolException e){
         	System.out.println(e);
 
         }
-        
-      
+
+
 }
 }
--- WeatherWeb/src/main/webapp/index.jsp
+++ WeatherWeb/src/main/webapp/index.jsp
@@ -4,20 +4,70 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>My WeatherWeb</title>
+<link rel="stylesheet" href="style2.css">
 </head>
 <body>
- <h1> ${date} </h1>
- <h1> ${tempreature} </h1>
- <h1> ${humidity} </h1>
- <h1> ${windSpeed} </h1>
- <h1> ${weatherCondition} </h1>
+ <div class="imagb">  </div>
+ 
+
+<div class="card">
+     <p class="city">${CityName}</p>
+
+
+	  <p class="date"> ${date}  <span> ${time} </span></p>
+	  <div class="box">
+	    <img src="./images/cityscape.png" >
+	  </div>
+	
+	    
+	    
+	 
+		 <div class="imagT">
+		     <img src="./images/warm-up.png" >
+		 </div>
+		  <p class="temp">  ${tempreature}<sup>c</sup></p>
+		  
+		  <div class="imagH">
+		     <img src="./images/humidity.png" >
+		 </div>
+		  <p class="humi">${humidity}<sup>g.m<sup>-3</sup></sup></p>
+		  
+		  
+		   <div class="imagW">
+		     <img src="./images/wind.png" >
+		 </div>
+		  <p class="wind"> ${windSpeed}<sup>knot</sup> </p>
+		
+		
+		<div class="imagWea">
+		     <img src="./images/weatherC.png" >
+		 </div>
+        <p class="weather"> ${weatherCondition} </p>
+        
+        <div class="imagP">
+		     <img src="./images/pressure.png" >
+		 </div>
+        <p class="Pressure"> ${pressure} <sup>hPa</sup></p>
+        
+        
+         <div class="imagC">
+		     <img src="./images/earth.png" >
+		 </div>
+        <p class="country"> ${country}<sup>country</sup></p>
+       
+       
+       
+		 
+	  
+</div>
  
 
  
  
  
   
+   
   
   <!-- <h3>
   
